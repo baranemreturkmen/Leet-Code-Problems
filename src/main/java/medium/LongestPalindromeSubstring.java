01176f8af1dd12fb97df4005a110657e10b69d29
Baran Emre Turkmen <baranemre1997@gmail.com>
2023-07-24 14:16:29 +0300

    24.07.2023 LongestPalindrome class name changed.

--- src/main/java/medium/LongestPalindromeSubstring.java
+++ src/main/java/medium/LongestPalindromeSubstring.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class LongestPalindrome {
+public class LongestPalindromeSubstring {
 
     static List<String> stringList = new ArrayList<>();
 
