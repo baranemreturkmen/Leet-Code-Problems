21571f2e62196c9e26a27689f9d14df15f5f69b8
Baran Emre Turkmen <baranemre1997@gmail.com>
2023-07-26 08:33:19 +0300

    26.07.2023 Todo solved. Better performance solution implemented.

--- src/main/java/medium/LongestPalindromeSubstring.java
+++ src/main/java/medium/LongestPalindromeSubstring.java
@@ -3,11 +3,14 @@ package medium;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class LongestPalindromeSubstring {
 
     static List<String> stringList = new ArrayList<>();
+
+    //TODO: Measure 2 methods execution time. Is second approach implements better performance?
 
     public static void main(String[] args) {
         initializeExampleStrings();
@@ -16,9 +19,15 @@ public class LongestPalindromeSubstring {
         for(String string: stringList){
             System.out.println(resultOfLongestPalindrome(string));
         }
+
+        System.out.println("---------------------");
+
+        System.out.println("Result of longest palindrome substring with better performance: ");
+        for(String string: stringList){
+            System.out.println(resultOfLongestPalindromeWithBetterPerformance(string));
+        }
     }
 
-    //TODO: For the case6, reduce the consuming time increase the performance!
     private static String resultOfLongestPalindrome(String s){
         List<Character> charList = s.chars()
                 .mapToObj(e->(char)e).collect(Collectors.toList());
@@ -55,6 +64,26 @@ public class LongestPalindromeSubstring {
                 max(Comparator.comparingInt(String::length)).get();
     }
 
+    private static String resultOfLongestPalindromeWithBetterPerformance(String s){
+        if(s.isBlank() || Objects.isNull(s)){
+            return "";
+        }
+
+        int start = 0;
+        int end = 0;
+
+        for(int i=0; i<s.length();i++){
+            int len1 = expandFromMiddle(s,i,i);
+            int len2 = expandFromMiddle(s,i,i+1);
+            int maxLen = Math.max(len1,len2);
+            if(maxLen > end - start){
+                start = i - ((maxLen-1)/2);
+                end = i + (maxLen/2);
+            }
+        }
+        return s.substring(start,end + 1);
+    }
+
     private static void initializeExampleStrings(){
         String s1 = "babad";
         String s2 = "cbbd";
@@ -75,6 +104,14 @@ public class LongestPalindromeSubstring {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(s);
         return stringBuilder.reverse().toString();
+    }
+
+    private static int expandFromMiddle(String s, int left, int right){
+        while (left >= 0 && right< s.length() && s.charAt(left)==s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
     }
 
 }
